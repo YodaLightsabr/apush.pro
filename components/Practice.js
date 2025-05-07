b5f2e09c9090bf4daa635f31b2fdec1cf3023a59
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2025-05-07 12:04:21 -0700

    Study lists

--- components/Practice.js
+++ components/Practice.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 
-import { Button, Card, Input, Loading, Page, Progress, Tabs, Text, Textarea } from '@geist-ui/core'
+import { Button, Card, Input, Loading, Page, Progress, Tabs, Text, Textarea, useToasts } from '@geist-ui/core'
 import { ArrowRight, HelpCircle, Plus } from "@geist-ui/icons";
 import { useEffect, useState } from "react";
 import ai from "@/lib/ai";
@@ -14,68 +14,70 @@ import Question from "./Question";
 
 
 export default function Practice({ name, questionBank }) {
-  const { displayUnit } = useSync();
-  const [question, setQuestion] = useState(null);
-  const [completedQuestions, setCompletedQuestions] = useState([]);
-  const [stats, setStats] = useState({
-    correct: 0,
-    partial: 0,
-    incorrect: 0,
-  });
+    const { setToast } = useToasts()
+
+    const { displayUnit, setSavedQuestions } = useSync();
+    const [question, setQuestion] = useState(null);
+    const [completedQuestions, setCompletedQuestions] = useState([]);
+    const [stats, setStats] = useState({
+        correct: 0,
+        partial: 0,
+        incorrect: 0,
+    });
 
 
-  const [state, setState_raw] = useState("loading");
-  const [stateUpdatedAt, setStateUpdatedAt] = useState(0);
-  const setState = (newState) => {
-    setState_raw(newState);
-    setStateUpdatedAt(Date.now());
-  }
-
-  const [answer, setAnswer] = useState("");
-
-
-  const nextQuestion = () => {
-    if (question?.question) {
-      setCompletedQuestions(completedQuestions => {
-        return [...completedQuestions, question.question];
-      });
+    const [state, setState_raw] = useState("loading");
+    const [stateUpdatedAt, setStateUpdatedAt] = useState(0);
+    const setState = (newState) => {
+        setState_raw(newState);
+        setStateUpdatedAt(Date.now());
     }
 
-    setState("loading");
-    setAnswer("");
-
-    const unit = localStorage.getItem("unit");
-
-    const availableQuestions = questionBank.filter(potentialQuestion => {
-      if (completedQuestions.includes(potentialQuestion.question)) return false;
-      if (unit == "all") return true;
-      const unitFilter = unit == "1/2" ? 1 : +unit;
-
-      return potentialQuestion.unit == unitFilter;
-    })
-
-    const nextQuestion = availableQuestions[Math.floor(Math.random() * availableQuestions.length)];
-    const questionText = nextQuestion.questions[Math.floor(Math.random() * nextQuestion.questions.length)];
-
-    setQuestion(currentQuestion => {
-      return {
-        number: (currentQuestion?.number || 0) + 1,
-        question: questionText,
-        unit: nextQuestion.unit,
-      }
-    });
-    setState("question");
-  }
-
-  useEffect(() => {
-    nextQuestion();
-  }, []);
+    const [answer, setAnswer] = useState("");
 
 
-  const checkAnswer = async (studentAnswer = answer) => {
-    setState("loadingAnswer");
+    const nextQuestion = () => {
+        if (question?.question) {
+            setCompletedQuestions(completedQuestions => {
+                return [...completedQuestions, question.question];
+            });
+        }
 
-    const prompt = `
+        setState("loading");
+        setAnswer("");
+
+        const unit = localStorage.getItem("unit");
+
+        const availableQuestions = questionBank.filter(potentialQuestion => {
+            if (completedQuestions.includes(potentialQuestion.question)) return false;
+            if (unit == "all") return true;
+            const unitFilter = unit == "1/2" ? 1 : +unit;
+
+            return potentialQuestion.unit == unitFilter;
+        })
+
+        const nextQuestion = availableQuestions[Math.floor(Math.random() * availableQuestions.length)];
+        const questionText = nextQuestion.questions[Math.floor(Math.random() * nextQuestion.questions.length)];
+
+        setQuestion(currentQuestion => {
+            return {
+                number: (currentQuestion?.number || 0) + 1,
+                question: questionText,
+                unit: nextQuestion.unit,
+            }
+        });
+        setState("question");
+    }
+
+    useEffect(() => {
+        nextQuestion();
+    }, []);
+
+
+    const checkAnswer = async (studentAnswer = answer) => {
+        setState("loadingAnswer");
+
+        const prompt = `
 A student was given an AP US History question: "${question.question}".
 
 The student answered "${studentAnswer}".
@@ -93,102 +95,110 @@ You MUST (ABSOLUTELY MUST) respond in JSON format (NO BACKTICKS) with the follow
   
 Do not include any other text or explanation. The JSON should look like this: { "status": "correct", "feedback": "...", "review": "..." }`;
 
-    const response = await ai(prompt);
-    const json = JSON.parse(response);
+        const response = await ai(prompt);
+        const json = JSON.parse(response);
 
-    setQuestion(question => {
-      question[json.status] = true;
-      question.feedback = json.feedback;
-      question.answer = studentAnswer;
-      question.review = json.review;
-      return question;
-    });
+        setQuestion(question => {
+            question[json.status] = true;
+            question.feedback = json.feedback;
+            question.answer = studentAnswer;
+            question.review = json.review;
+            return question;
+        });
 
-    setStats(stats => {
-      return {
-        ...stats,
-        [json.status]: stats[json.status] + 1,
-      };
-    });
+        setStats(stats => {
+            return {
+                ...stats,
+                [json.status]: stats[json.status] + 1,
+            };
+        });
 
-    setState("answer");
+        setState("answer");
 
-  }
+    }
 
-  const { sm } = useMedia();
-
-
-  return (
-    <>
-      <div>
-        <Text h2 mb={0} mt={0}>APUSH<em>.pro</em> {name}</Text>
-        <Text h3 mt={0} mb={3} style={{
-          color: "#555",
-        }}>{displayUnit || "All Units"}</Text>
+    const { sm } = useMedia();
 
 
-        {question && <Question question={question} onExplain={() => {
-          setAnswer("Please explain this for me");
-          checkAnswer("Please explain this for me");
-        }} loading={state == "loadingAnswer"} startedLoadingAt={stateUpdatedAt} />}
+    return (
+        <>
+            <div>
+                <Text h2 mb={0} mt={0}>APUSH<em>.pro</em> {name}</Text>
+                <Text h3 mt={0} mb={3} style={{
+                    color: "#555",
+                }}>{displayUnit || "All Units"}</Text>
 
 
-        <div style={{
-          position: "fixed",
-          bottom: "0px",
-          width: sm ? "calc(100% - 6rem)" : "calc(100% - 2rem)",
-          maxWidth: "800px",
-          left: "50%",
-          transform: "translateX(-50%)",
-          padding: "1rem",
-          paddingBottom: "0.5rem",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: "white",
-          boxShadow: "0px 0px 25px 31px rgba(255,255,255,1)",
-          zIndex: "100",
+                {question && <Question question={question} onExplain={() => {
+                    setAnswer("Please explain this for me");
+                    checkAnswer("Please explain this for me");
+                }} loading={state == "loadingAnswer"} onSave={() => {
+                    setSavedQuestions(questions => {
+                        if (questions.find(q => q.question == question.question)) return questions;
+                        return [...questions, question];
+                    });
+                    setToast({
+                        text: "Saved to your study list",
+                    })
+                }} startedLoadingAt={stateUpdatedAt} />}
 
-        }}>
-          <Textarea value={answer} onChange={e => setAnswer(e.target.value)} width="100%" scale={4 / 3} placeholder="Your answer" disabled={state != "question"} onKeyDown={e => {
-            if (e.key === "Enter") {
-              e.preventDefault();
-              checkAnswer();
-            }
-          }} style={{
-            width: "100%"
-          }}></Textarea>
-          <Text small mt={"4px"} style={{
-            width: "800px",
-            maxWidth: "100%",
-            textAlign: "left",
-            opacity: "0.8",
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "start",
-            alignItems: "center",
-            gap: "0.5rem",
-          }}>
-            <Text type="success" span>{stats.correct} correct</Text> •
-            <Text type="warning" span>{stats.partial} partial</Text> •
-            <Text type="error" span>{stats.incorrect} incorrect</Text>
-          </Text>
-          {state == "answer" ? <Button auto onClick={nextQuestion} icon={<ArrowRight />} type="success-light" style={{
-            position: "absolute",
-            right: "max(calc(50% - 400px + 1rem), 2rem)",
-            top: "43%",
-            transform: "translateY(-50%)",
-          }}>Next Question</Button> : null}
-        </div>
 
-        <div style={{
-          height: "200px",
-          width: "100%",
-        }}></div>
+                <div style={{
+                    position: "fixed",
+                    bottom: "0px",
+                    width: sm ? "calc(100% - 6rem)" : "calc(100% - 2rem)",
+                    maxWidth: "800px",
+                    left: "50%",
+                    transform: "translateX(-50%)",
+                    padding: "1rem",
+                    paddingBottom: "0.5rem",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    backgroundColor: "white",
+                    boxShadow: "0px 0px 25px 31px rgba(255,255,255,1)",
+                    zIndex: "100",
 
-      </div>
-    </>
-  );
+                }}>
+                    <Textarea value={answer} onChange={e => setAnswer(e.target.value)} width="100%" scale={4 / 3} placeholder="Your answer" disabled={state != "question"} onKeyDown={e => {
+                        if (e.key === "Enter") {
+                            e.preventDefault();
+                            checkAnswer();
+                        }
+                    }} style={{
+                        width: "100%"
+                    }}></Textarea>
+                    <Text small mt={"4px"} style={{
+                        width: "800px",
+                        maxWidth: "100%",
+                        textAlign: "left",
+                        opacity: "0.8",
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "start",
+                        alignItems: "center",
+                        gap: "0.5rem",
+                    }}>
+                        <Text type="success" span>{stats.correct} correct</Text> •
+                        <Text type="warning" span>{stats.partial} partial</Text> •
+                        <Text type="error" span>{stats.incorrect} incorrect</Text>
+                    </Text>
+                    {state == "answer" ? <Button auto onClick={nextQuestion} icon={<ArrowRight />} type="success-light" style={{
+                        position: "absolute",
+                        right: "max(calc(50% - 400px + 1rem), 2rem)",
+                        top: "43%",
+                        transform: "translateY(-50%)",
+                    }}>Next Question</Button> : null}
+                </div>
+
+                <div style={{
+                    height: "200px",
+                    width: "100%",
+                }}></div>
+
+            </div>
+        </>
+    );
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,47 @@
 import useMedia from "@/lib/media";
 import { SyncProvider, useSync } from "@/lib/sync";
 import "@/styles/globals.css";
-import { GeistProvider, CssBaseline, Text, Page, Button, Drawer } from '@geist-ui/core'
+import { GeistProvider, CssBaseline, Text, Page, Button, Drawer, Card } from '@geist-ui/core'
 import { ChevronUpDown, Circle, Settings } from "@geist-ui/icons";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function StudyListButton() {
   const [state, setState] = useState(false);
+  const { savedQuestions } = useSync();
+  useEffect(() => {
+    if (!Array.isArray(savedQuestions)) {
+      setSavedQuestions([]);
+    }
+  }, []);
   return (
     <>
       <Button onClick={() => setState(true)} icon={<ChevronUpDown />} auto>Study List</Button>
       <Drawer visible={state} onClose={() => setState(false)} placement="right">
         <Drawer.Title>Study List</Drawer.Title>
-        <Drawer.Subtitle>0 items</Drawer.Subtitle>
-        <Drawer.Content>
-          <p>Get started by adding items to your study list</p>
+        <Drawer.Subtitle>{savedQuestions?.length || 0} item{savedQuestions?.length == 1 ? "" : "s"}</Drawer.Subtitle>
+        <Drawer.Content style={{
+          maxWidth: "40vw"
+        }}>
+          {savedQuestions?.length == 0 ? <p>Get started by adding items to your study list</p> : null}
+          {Array.isArray(savedQuestions) ? (savedQuestions).map((question, index) => (
+            <Card key={index} marginBottom={1}>
+              <Text h4 margin={0}>{question.question}</Text>
+              <div style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+                gap: "1rem",
+                marginTop: "1rem",
+              }}>
+                <Text margin={0}>Unit {question.unit}</Text>
+                {question.correct && <Text margin={0} type="success">Correct</Text>}
+                {question.partial && <Text margin={0} type="warning">Partial</Text>}
+                {question.incorrect && <Text margin={0} type="error">Incorrect</Text>}
+              </div>
+            </Card>
+          )) : null}
         </Drawer.Content>
       </Drawer>
 
